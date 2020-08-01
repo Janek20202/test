0cb0fce434c62472d841d7a88ca450e7743afa47
Janek20202 <69000093+Janek20202@users.noreply.github.com>
2020-08-01 20:59:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1903,10 +1903,10 @@
      */
     DistanceMeter.config = {
         // Number of digits.
-        MAX_DISTANCE_UNITS: 5,
+        MAX_DISTANCE_UNITS: 15,
 
         // Distance that causes achievement animation.
-        ACHIEVEMENT_DISTANCE: 100,
+        ACHIEVEMENT_DISTANCE: 1000,
 
         // Used for conversion from pixel distance to a scaled unit.
         COEFFICIENT: 0.025,
